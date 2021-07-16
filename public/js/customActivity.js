a2c0ce76a9ea252fdb7ec6a9a6db4b7437055dcf
Shweta Gaikwad <sgaikwad@tracelink.com>
2021-07-16 19:02:56 +0530

    configuration changes

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,8 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.SMS_Test.PhoneNumber}}" //<----This should map to your data extension name and phone number column
+           // "to": "{{Contact.Attribute.SMS_Test.PhoneNumber}}" //<----This should map to your data extension name and phone number column
+           "to": "918976734094"
         }];
 
         payload['metaData'].isConfigured = true;
